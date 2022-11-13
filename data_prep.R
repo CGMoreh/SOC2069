43ae0c9e0bc77d6aeb02ae7778b26a5a018cd3b3
Chris Moreh <43904262+CGMoreh@users.noreply.github.com>
2022-11-13 08:26:36 +0000

    adds

--- data_prep.R
+++ data_prep.R
@@ -20,48 +20,46 @@ path_office <- "A:/OneDrive - Newcastle University/WORK Newcastle/SOC2069/Data/6
 path_hp_laptop <- "C:/Users/ncm281/OneDrive - Newcastle University/WORK Newcastle/SOC2069/Data/6614stata_5FAC8DA9C415588B7AFA54AB180477074A808CCA1CD9354267911054D02007D4_V1/UKDA-6614-stata/stata/stata13_se/ukhls/h_indresp.dta"
 
 ## Load original dataset
-us <- read_stata(path_hp_laptop)
-
-## Set all negative values to NA; see: https://www.understandingsociety.ac.uk/documentation/mainstage/user-guides/main-survey-user-guide/missing-values
-us2 <- us %>%  
-  set_na(na = c(-1:-21)) %>%            # Set negative coded missing values to NA  
-  select(where(~mean(is.na(.)) < 0.6))  # Remove columns with more than 40% missing
-
-us3 <- us2 %>% select(
-  pidp, h_ivfio, h_ioutcome, # id, outcome
-  h_sex, h_nchunder16, h_nchresp, 	h_nadoptch, h_nnatch, # sex, children
-  h_istrtdaty:h_istrtdatd, # interview date
-  h_lkmove:h_netpuse, # house move, economic activity, car, phone, computers, internet
-  h_oprlg:h_oprlg3, # religion
-  h_health:h_aidxhh, # health, caring for someone
-  h_mstatsam, h_currmstat,
-  h_jbhas:h_jbterm1, h_jbsemp, h_jbmngr, h_jbsect:h_jbsat, h_tujbpl, h_wkends:h_jbflex96, # work
-  h_depenth1:h_depenth6, h_jbsec, h_j2has:h_trbikefq, h_ynotbike3, h_pncars:h_carbuy97,
-  h_benbase1:h_bensta96, # benefits
-  h_debt1:h_debt96, # debts
-  h_finnow:h_save, # finances
-  h_hubuys:h_hudiy, # Who does...
-  h_huboss, h_howlng,
-  h_scsf1, h_scghqa:h_scghql, 
-  h_sclfsat1:h_sclfsato,
-  h_eumem, h_scwhorupro:h_scwhoruage,
-  h_marstat, h_employ, h_ppsex, h_hhsize, 
-  h_fimngrs_dv, h_paygl:h_paynu_dv, 
-  h_age_dv, h_doby_dv, h_marstat_dv, h_ethn_dv, h_fimnnet_dv, h_country, h_urban_dv, h_agegr10_dv,
-  h_nchild_dv, h_ndepchl_dv, h_hiqual_dv, h_jbft_dv, h_jbes2000, h_jbnssec8_dv:h_jbnssec3_dv,  # various personal
-  h_scghq1_dv:h_jwbs2_dv # scales
-)
-
-us3 <- us3 %>% 
+us <- read_stata(path_hp_laptop) %>% 
+  set_na(na = c(-1:-21)) %>%                 # Set negative coded missing values to NA  
+  select(where(~mean(is.na(.)) < 0.6)) %>%   # Remove columns with more than 40% missing
+  select(
+    pidp, h_ivfio, h_ioutcome, # id, outcome
+    h_sex, h_nchunder16, h_nchresp, 	h_nadoptch, h_nnatch, # sex, children
+    h_istrtdaty:h_istrtdatd, # interview date
+    h_lkmove:h_netpuse, # house move, economic activity, car, phone, computers, internet
+    h_oprlg:h_oprlg3, # religion
+    h_health:h_aidxhh, # health, caring for someone
+    h_mstatsam, h_currmstat,
+    h_jbhas:h_jbterm1, h_jbsemp, h_jbmngr, h_jbsect:h_jbsat, h_tujbpl, h_wkends:h_jbflex96, # work
+    h_depenth1:h_depenth6, h_jbsec, h_j2has:h_trbikefq, h_ynotbike3, h_pncars:h_carbuy97,
+    h_benbase1:h_bensta96, # benefits
+    h_debt1:h_debt96, # debts
+    h_finnow:h_save, # finances
+    h_hubuys:h_hudiy, # Who does...
+    h_huboss, h_howlng,
+    h_scsf1, h_scghqa:h_scghql, 
+    h_sclfsat1:h_sclfsato,
+    h_eumem, h_scwhorupro:h_scwhoruage,
+    h_marstat, h_employ, h_ppsex, h_hhsize, 
+    h_fimngrs_dv, h_paygl:h_paynu_dv, 
+    h_age_dv, h_doby_dv, h_marstat_dv, h_ethn_dv, h_fimnnet_dv, h_country, h_urban_dv, h_agegr10_dv,
+    h_nchild_dv, h_ndepchl_dv, h_hiqual_dv, h_jbft_dv, h_jbes2000, h_jbnssec8_dv:h_jbnssec3_dv,  # various personal
+    h_scghq1_dv:h_jwbs2_dv # scales
+    )  %>% 
   labelled::remove_attributes("format.stata") %>% 
-  drop_labels()
+  drop_labels() %>% 
+  mutate_if(is.factor , as_label)
 
-us4 <- us3 %>% mutate_if(is.factor , as_label)
+saveRDS(us, file = "docs/Data/ukhls-w8.rds")
+summarytools::dfSummary(us) %>% print(file = "docs/Data/varlist.html")
 
 
-# us3 %>% mutate_if(is.factor , as_character) %>% write.csv(file = "docs/Data/us_csv.csv")
+# Further wrangling
 
-saveRDS(us3, file = "docs/Data/ukhls-w8.rds")
+ukhls_w8 <- readRDS("C:/Users/ncm281/Documents/SOC2069/docs/Data/ukhls-w8.rds")
+
+ukhls_w8 <- ukhls_w8 %>% rename_all(~stringr::str_replace(.,"^h_",""))
 
 # Small data for testing
 
