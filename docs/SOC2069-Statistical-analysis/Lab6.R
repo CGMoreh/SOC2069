6fa85ce6bd9dd956b3fea63eaf872d1941ad2976
Chris Moreh <43904262+CGMoreh@users.noreply.github.com>
2022-11-14 01:09:32 +0000

    adds

---
+++ docs/SOC2069-Statistical-analysis/Lab6.R
@@ -0,0 +1,33 @@
+#####################################################################################
+###     Lab 6             ###########################################################
+###     Week 11           ###########################################################
+#####################################################################################
+
+# This is an R script
+# You can type, save and run your commands here
+# You can also write comments by typing hashtag (#) followed by the text of your comment (like these comments you are reading now)
+# Anything that is not preceded by a hashtag is recognised by R as an executable command and will try to execute it if you run the script; like the one below:
+
+1 + 3 - 5 + 7
+
+# To execute the command above, you can either place the cursor anywhere on that line (or select the command lines) and click CNTRL+ENTER, or click on the Run button in this script window's toolbar above.
+
+# Results from executed commands are printed in the Console window (the one below, by default)
+# The command above performs the arithmetic operation and results in the number 6
+
+# Let's start by installing and loading some useful user-written packages using the 'install_packages' and 'library' functions included in base-R packages. Run the commands below:
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+# We now have access to functions contained in the 'tidyverse' bundle of packages. 
+
+# Below you can type your own commands, or you can copy/paste commands from the Lab6 online worksheet and execute them here
+
+
+
+
+
+
+
