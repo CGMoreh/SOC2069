c18367a21f760bb4fae1b47a13fc73e0a68accf0
Chris Moreh <ncm281@newcastle.ac.uk>
2022-11-21 05:18:07 +0000

    add

--- docs/Worksheets/Lab7.R
+++ docs/Worksheets/Lab7.R
@@ -35,7 +35,7 @@ mosaic::max( ~ scghq1_dv, data = ukhls, na.rm = TRUE)
 #  print(maxvalue)                                   # print `maxvalue` to the console to check it
 #  
 #  ukhls <- ukhls %>%
-#    mutate(wellbeing2 = maxvalue-scghq1_dv)          # use `maxvalue` instead of 36
+#    mutate(wellbeing = maxvalue-scghq1_dv)          # use `maxvalue` instead of 36
 
 ## ----results='hold'-----------------------------------------------------------
 favstats( ~ scghq1_dv, data = ukhls)
@@ -51,4 +51,76 @@ xyplot(wellbeing ~ age_dv, data = ukhls)
 
 ## -----------------------------------------------------------------------------
 xyplot(wellbeing ~ age_dv, type = c("p", "r"), data = ukhls)
+
+## -----------------------------------------------------------------------------
+xyplot(wellbeing ~ age_dv, 
+       type = c("p", "r"), 
+       col.line = "red", 
+       data = ukhls)
+
+## -----------------------------------------------------------------------------
+cor.test(wellbeing ~ age_dv, data = ukhls)
+
+## -----------------------------------------------------------------------------
+model1 <- lm(wellbeing ~ 1 + age_dv, data = ukhls)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  model1 <- lm(wellbeing ~ age_dv, ukhls)
+
+## -----------------------------------------------------------------------------
+coef(model1)
+
+## -----------------------------------------------------------------------------
+lm(wellbeing ~ 1, data = ukhls) %>% coef()
+
+mean( ~ wellbeing, data = ukhls, na.rm = TRUE)
+
+## -----------------------------------------------------------------------------
+24.3470 + (0.0109 * 21)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  # First, extract the median age value from the data using the `median()` function:
+#  
+#  medianAge <- median( ??? )
+#  
+#  # Then `print` the median to check it and see if it's correct
+#  
+#  ???
+#  
+#  # Then use the `mutate` function to create the new variables called "age_median_centred" by subtracting the median age from all values of `age_dv`
+#  
+#  ???
+
+## ----echo=F-------------------------------------------------------------------
+medianAge <- median( ~ age_dv, data = ukhls, na.rm = TRUE)
+
+ukhls <- ukhls %>% 
+  mutate(age_median_centred = age_dv - medianAge)
+
+## -----------------------------------------------------------------------------
+favstats( ~ age_median_centred, data = ukhls)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  # Complete the lm() command
+#  
+#  model2 <- ????
+#  
+#  # Then print the coefficients from model2
+#  
+#  
+
+## ----echo=FALSE---------------------------------------------------------------
+model2 <- lm(wellbeing ~ age_median_centred, ukhls)
+coef(model2)
+
+## -----------------------------------------------------------------------------
+summary(model2)
+
+## -----------------------------------------------------------------------------
+summary(model2) %>% rsquared %>% sqrt()
+
+cor.test(wellbeing ~ age_median_centred, data = ukhls)$estimate
+
+## ----eval=F-------------------------------------------------------------------
+#  options(scipen = 999)
 
