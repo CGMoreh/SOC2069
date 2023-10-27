7a67a6e7001387fda89ba3180b51f82db7539a31
Chris Moreh <43904262+CGMoreh@users.noreply.github.com>
2023-10-27 11:05:20 +0100

    add

--- .Private/reg_sims.R
+++ .Private/reg_sims.R
@@ -1,7 +1,10 @@
 library(tidyverse)
 library(easystats)
 library(ggformula)
+library(ggeffects)
 
+
+## Population data simulation --------------------------------------------------------------------------
 
 set.seed(123)
 
@@ -34,3 +37,52 @@ gf_point(y ~ x, data = df_reg_sample) |> gf_lm()
 
 
 lm(y ~ x, data = df_reg_sample) |> model_parameters()
+
+
+
+## Interaction effect simulation (numeric/indicator*indicator) ------------------------------------------------------
+
+set.seed(123)
+
+n <-  80
+
+# simulate a covariate
+
+# x <- rnorm(n, 0, 1)
+
+x <- sample(c(0,1), size = n, replace = T)
+
+# simualate a binary moderator
+z <- sample(c(0,1), size = n, replace = T)
+
+beta0 <- 77.320
+beta1 <- 0.141
+beta2 <- 1.863
+beta3 <- -14.007
+sigma0 <- 1.2
+
+# mu
+mu <- beta0 + beta1*x + beta2*z + beta3*(x*z) 
+
+y <- rnorm(n, mu, sigma0)
+
+dd <- data.frame(y=y, x=x, z=z)
+
+mod <- lm(y ~ x * z, data = dd) 
+
+compare_parameters(mod)
+model_parameters(mod)
+
+ggeffects::ggpredict(mod,
+                     terms = c("x", "z")) |> plot()
+
+
+
+
+## Simulate data for a mixed model ----------------------------------------------------------------------------------
+
+See: https://stats.stackexchange.com/questions/488188/what-are-the-steps-to-simulate-data-for-a-linear-model-with-random-slopes-and-ra
+
+
+
+
